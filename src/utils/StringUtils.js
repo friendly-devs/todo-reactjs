30f945ba42a469a5f415c090459d1233848d5a45
tamvv <vovantam.dev@gmail.com>
2021-04-13 14:45:51 +0700

    Add slugify

--- src/utils/StringUtils.js
+++ src/utils/StringUtils.js
@@ -1,8 +1,19 @@
+import slugify from "slugify";
+
 const Utils = {
   includesIgnoreCase(target, value) {
     const lowCaseTarget = target.toLocaleLowerCase();
     const lowCaseValue = value.toLocaleLowerCase();
     return lowCaseTarget.includes(lowCaseValue);
+  },
+  toSlug(str) {
+
+    return slugify(str, {
+      replacement: "-",
+      lower: true,
+      remove: /[*+~.,()'"!:@]/g,
+      locale: "vi",
+    });
   },
 };
 
--- src/utils/StringUtils.test.js
+++ src/utils/StringUtils.test.js
@@ -1,13 +1,21 @@
-import Utils from './StringUtils'
+import Utils from "./StringUtils";
 
-test('it shout include', () => {
-  expect(Utils.includesIgnoreCase('same', 'same')).toBe(true);
+test("it shout include", () => {
+  expect(Utils.includesIgnoreCase("same", "same")).toBe(true);
 });
 
-test('it shout include ignore case', () => {
-  expect(Utils.includesIgnoreCase('Tra cuu', 'tra cuu')).toBe(true);
+test("it shout include ignore case", () => {
+  expect(Utils.includesIgnoreCase("Tra cuu", "tra cuu")).toBe(true);
 });
 
-test('it shout include ignore case and different text', () => {
-  expect(Utils.includesIgnoreCase('Tra cứu', 'cứu')).toBe(true);
+test("it shout include ignore case and different text", () => {
+  expect(Utils.includesIgnoreCase("Tra cứu", "cứu")).toBe(true);
+});
+
+test("it shout to slug low case", () => {
+  expect(Utils.toSlug(' Đây Là  Thông tIn  ')).toBe('day-la-thong-tin');
+});
+
+test("it shout to slug remove special character", () => {
+  expect(Utils.toSlug(' Đây Là  Thông. tIn ,')).toBe('day-la-thong-tin');
 });
